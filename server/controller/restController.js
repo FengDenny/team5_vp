daa276ed402e794929fd150014b6a5c2a1c8fb82
FengDenny <fengdennyy@gmail.com>
2021-03-18 14:04:12 -0700

    Delete restController.js

--- server/controller/restController.js
+++
@@ -1,7 +0,0 @@
-const db = require("../config/dbConfig");
-exports.getAllRestaurants = async (req, res, next) => {
-  db.query("SELECT * FROM restaurants;").then(([results, fields]) => {
-    console.log(results);
-    res.send(results);
-  });
-};
