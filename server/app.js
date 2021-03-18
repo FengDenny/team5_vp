952aaea77dca284ffbc51401441af92a1baeaa32
FengDenny <fengdennyy@gmail.com>
2021-03-18 14:03:43 -0700

    search route

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const restaurantRoutes = require("./routes/restaurant");
+const path = require("path");
+const restaurantRoutes = require("./routes/restaurants");
+const searchRoutes = require("./routes/search");
+// Error handling
+const AppError = require("./utility/AppError");
+const GlobalErrorHandler = require("./controller/errorController");
 
 // express app initialized
 const app = express();
@@ -8,6 +13,9 @@ const app = express();
 // Body Parser
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// for image upload
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 // development logging
 if (process.env.NODE_ENV == "development") {
@@ -18,5 +26,14 @@ if (process.env.NODE_ENV == "development") {
 
 // routes
 app.use("/api/v1/restaurants", restaurantRoutes);
+app.use("/api/v1/search", searchRoutes);
+
+// route middleware
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 401));
+});
+
+// Global Error Handler for DB
+app.use(GlobalErrorHandler);
 
 module.exports = app;
