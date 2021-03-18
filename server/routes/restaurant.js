cd2a7a72538b855992b0545ea700375adcc1ec07
FengDenny <fengdennyy@gmail.com>
2021-03-18 14:04:30 -0700

    Delete restaurant.js

--- server/routes/restaurant.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const restController = require("../controller/restController");
-
-router.get("/getAllRestaurants", restController.getAllRestaurants);
-
-module.exports = router;
