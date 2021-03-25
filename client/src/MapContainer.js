5003ec832d3378909549bbc2b2aecdf70b9cfc1e
FengDenny <fengdennyy@gmail.com>
2021-03-25 11:03:06 -0700

    React hooks refactored

---
+++ client/src/MapContainer.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, Component } from "react";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+
+/**
+ * After VP: make this so I can pass a parameter of address to get that map
+ */
+
+const mapStyles = {
+  margin: "auto",
+  width: "50%",
+  height: "50%",
+};
+
+function MapContainer(props) {
+  const [showInfoWindow, setShowInfoWindow] = useState(false);
+  const [activeMarker, setActiveMarker] = useState({});
+  const [selectedPlace, setSelectedPlace] = useState({});
+
+  const onMarkerClick = (props, marker) => {
+    setSelectedPlace(props);
+    setActiveMarker(marker);
+    setShowInfoWindow(true);
+  };
+
+  const onClose = () => {
+    if (showInfoWindow) {
+      setShowInfoWindow(false);
+      setActiveMarker(null);
+    }
+  };
+
+  const { google } = props;
+
+  return (
+    <Map
+      google={google}
+      zoom={15}
+      style={mapStyles}
+      initialCenter={{
+        lat: 37.72424252726849,
+        lng: -122.48015507898837,
+      }}
+    >
+      <Marker onClick={onMarkerClick} name={"SFSU"} />
+      <InfoWindow
+        marker={activeMarker}
+        visible={showInfoWindow}
+        onClose={onClose}
+      >
+        <div>
+          <h4>{selectedPlace.name}</h4>
+        </div>
+      </InfoWindow>
+    </Map>
+  );
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyB-2B4T7YdkI-_RSKtU6IA_h5XHILEi7QY",
+})(MapContainer);
